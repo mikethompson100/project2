b7a6719020f5348afa39449653bffcf99ab94ce0
Michael Thompson <mikethompson100@hotmail.com>
2022-03-23 20:02:47 -0400

    Added axios and performed first data retieval test from the API

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@
 const button1 = document.querySelector('#button1');
 const getAnimalsNumber = document.querySelector('input')
 
+button1.addEventListener('click', async () => {
+    let number = getAnimalsNumber.value;    
+    number = parseInt(number);
+    let response = await axios.get(
+        `https://zoo-animal-api.herokuapp.com/animals/rand/${number}`
+      );
+      console.log(response)
+})
